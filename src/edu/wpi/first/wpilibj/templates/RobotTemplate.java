f49f69e1aae041e875e735fd9e79895b9c93a6af
Stephen Johnson <>
2014-02-14 15:04:26 -0600

    Near complete 2014 Testing Code

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -13,6 +13,13 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.image.ColorImage;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.Compressor;
+
+//
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -27,15 +34,25 @@ public class RobotTemplate extends IterativeRobot {
     private Jaguar frontLeft;
     private Jaguar backRight;
     private Jaguar backLeft;
+    private Jaguar launcher;
+    
+    private DoubleSolenoid arms;
+    
+    private Compressor compress;
+    
+    private boolean freeMode;
+    
+    private AxisCamera camera;
     
     private Gyro thingy;
     
-    private Double speed;
+    private double speed;
     
     private SmartDashboard dash;
     
     private Joystick leftJoystick;
     private Joystick rightJoystick;
+    private Joystick prevJoystick;
     
     private RobotDrive drive;
     /**
@@ -44,28 +61,38 @@ public class RobotTemplate extends IterativeRobot {
      */
     public void robotInit() {
 
-        frontRight = new Jaguar(3);
-        frontLeft = new Jaguar(4);
-        backRight = new Jaguar(1);
-        backLeft = new Jaguar(2);
         
-        speed = new Double(0.50);
+        compress = new Compressor(1,1);
         
-        thingy = new Gyro(1);
+        arms = new DoubleSolenoid(1,2);
         
+        launcher = new Jaguar(5);
+//        frontRight = new Jaguar(3);
+//        frontLeft = new Jaguar(4);
+//        backRight = new Jaguar(1);
+//        backLeft = new Jaguar(2);
+//        
+        speed = 0.00;
+        
+        freeMode = false;
+//        
+//        thingy = new Gyro(1);
+//        
         dash = new SmartDashboard();
+//        
+        leftJoystick = new Joystick(2);
+        rightJoystick = new Joystick(1);
         
-        leftJoystick = new Joystick(1);
-        rightJoystick = new Joystick(2);
-        
-        drive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
-        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-        drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
-        
-        
+        compress.start();
+//        
+//        drive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+//        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+//        drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+//        
+//        camera = AxisCamera.getInstance("10.36.95.11");
+//        
     }
-
-    /**
+    /*
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
@@ -74,22 +101,54 @@ public class RobotTemplate extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
+    
     public void teleopPeriodic() {
-     
-        //Set to mecanum drive. Last digit is set to the gyro, which we dont have so default 0
-
-        dash.putDouble("speed" , speed.doubleValue());
-        dash.putDouble("Gyro", thingy.getAngle());
         
-        if(leftJoystick.getRawButton(2))
-            drive.mecanumDrive_Cartesian(0, leftJoystick.getY(), leftJoystick.getX(), 0);
-        else
-            drive.mecanumDrive_Cartesian(leftJoystick.getX(), leftJoystick.getY(), 0, 0);
+        if(rightJoystick.getRawButton(4))
+            freeMode = !freeMode;
         
-        dash.putDouble("LeftFront", frontLeft.getSpeed());
-        dash.putDouble("LeftBack", backLeft.getSpeed());
-        dash.putDouble("RightFront", frontRight.getSpeed());
-        dash.putDouble("RightBack", backRight.getSpeed());
+        if(!freeMode){           
+            if(rightJoystick.getRawButton(3) && speed < 1)
+                speed += 0.10;
+            if(rightJoystick.getRawButton(2) && speed > -1)
+                speed -= 0.10;           
+            if(rightJoystick.getTrigger())
+                launcher.set(speed);
+            else
+                launcher.set(0);
+        }
+        boolean armMovement = false; 
+        
+        if (freeMode)
+            launcher.set(rightJoystick.getY());
+        
+        if (rightJoystick.getRawButton(6)){
+            arms.set(DoubleSolenoid.Value.kForward);
+            armMovement = true;
+        }
+        if (rightJoystick.getRawButton(7) && !armMovement){
+            arms.set(DoubleSolenoid.Value.kReverse);
+            armMovement = true;
+        }
+        
+        if (!armMovement)
+            arms.set(DoubleSolenoid.Value.kOff);
+        
+        dash.putDouble("launcherSpeed", speed);
+        dash.putBoolean("free mode", freeMode);
+        
+        //Set to mecanum drive. Last digit is set to the gyro, which we dont have so default
+//        dash.putDouble("Gyro", thingy.getAngle());
+//        
+//        if(leftJoystick.getRawButton(2))
+////            drive.mecanumDrive_Cartesian(0, leftJoystick.getY(), leftJoystick.getX(), 0);
+//        else
+//            drive.mecanumDrive_Cartesian(leftJoystick.getX(), leftJoystick.getY(), 0, 0);
+//        
+//        dash.putDouble("LeftFront", frontLeft.getSpeed());
+//        dash.putDouble("LeftBack", backLeft.getSpeed());
+//        dash.putDouble("RightFront", frontRight.getSpeed());
+//        dash.putDouble("RightBack", backRight.getSpeed());
         
     }
     
